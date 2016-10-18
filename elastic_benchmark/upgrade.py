12c0c16f4165f688812dc5b64878a9eefedcb354
lamarwhitej <lamarwhitej@yahoo.com>
2016-10-18 10:15:59 -0500

    Update upgrade.py

--- elastic_benchmark/upgrade.py
+++ elastic_benchmark/upgrade.py
@@ -184,6 +184,6 @@ def entry_point():
     differences = parse_uptime(cl_args.uptime)
     current_time = str(datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z"))
     print current_time
-    differences.update(current_time)
+    differences.update({"done_time": current_time})
     print differences
     #esc.index(scenario_name='test_upgrade', env='osa_onmetal', **differences)
