e0366237eeb3003f069689410a80dac4f0708b89
lamarwhitej <lamarwhitej@yahoo.com>
2016-10-18 23:44:08 -0500

    Update upgrade.py

--- elastic_benchmark/upgrade.py
+++ elastic_benchmark/upgrade.py
@@ -67,7 +67,7 @@ def parse_during(output):
     return {"{0}_during".format(k): v.get("uptime_pct") for k, v in data.items()}
 
 def parse_persistence(output):
-    data = json.dumps(open(output).read())
+    data = json.load(open(output).read())
     body = {}
  
     for k,v in data:
