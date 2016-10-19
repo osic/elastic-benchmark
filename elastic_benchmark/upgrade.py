b20af0fd935c64f421006117f017a139429020c9
lamarwhitej <lamarwhitej@yahoo.com>
2016-10-18 23:14:31 -0500

    Update upgrade.py

--- elastic_benchmark/upgrade.py
+++ elastic_benchmark/upgrade.py
@@ -70,6 +70,7 @@ def parse_persistence(output):
     data = json.loads(open(output).read())
     body = {}
     
+    print {"{0}_during".format(k): v.get("create") for k, v in data.items()}
     for k,v in data.items():
         for s in v['create']:
             body.update({s['service']: s['create']})
