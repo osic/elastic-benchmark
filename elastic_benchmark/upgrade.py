b16764e269a378558f4e8402c5d366083ff4a209
lamarwhitej <lamarwhitej@yahoo.com>
2016-10-19 15:26:04 -0500

    Update upgrade.py

--- elastic_benchmark/upgrade.py
+++ elastic_benchmark/upgrade.py
@@ -157,15 +157,15 @@ class ArgumentParser(argparse.ArgumentParser):
 
         self.add_argument(
             "-u", "--uptime", metavar="<uptime output>",
-            required=True, default=None, help="A link to the uptime output from the upgrade.")
+            required=False, default=None, help="A link to the uptime output from the upgrade.")
 
         self.add_argument(
             "-d", "--during", metavar="<during output>",
-            required=True, default=None, help="A link to the during output from the upgrade.")
+            required=False, default=None, help="A link to the during output from the upgrade.")
         
         self.add_argument(
             "-p", "--persistence", metavar="<persistence test output>",
-            required=True, default=None, help="A link to the persistence test output from the upgrade.")
+            required=False, default=None, help="A link to the persistence test output from the upgrade.")
         
         self.add_argument(
             "-l", "--logs", metavar="<log link>",
@@ -203,8 +203,8 @@ def entry_point():
     before = parse(cl_args.before)
     after = parse(cl_args.after)
     differences = parse_differences(before, after)
-    differences.update(parse_uptime(cl_args.uptime))
-    differences.update(parse_during(cl_args.during))
-    differences.update(parse_persistence(cl_args.persistence))
+    #differences.update(parse_uptime(cl_args.uptime))
+    #differences.update(parse_during(cl_args.during))
+    #differences.update(parse_persistence(cl_args.persistence))
     differences.update({"done_time": current_time})
-    esc.index(scenario_name='upgrade', env='osa_onmetal', **differences)
+    #esc.index(scenario_name='upgrade', env='osa_onmetal', **differences)
