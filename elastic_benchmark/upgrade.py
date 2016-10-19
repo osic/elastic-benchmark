d4870401a621d451496c2383a6bdff2db75def42
lamarwhitej <lamarwhitej@yahoo.com>
2016-10-18 21:54:41 -0500

    Update upgrade.py

--- elastic_benchmark/upgrade.py
+++ elastic_benchmark/upgrade.py
@@ -70,7 +70,7 @@ def parse_persistence(output):
     data = json.loads(open(output).read())
     body = None
    
-    print {"{0}_resources_persistence".format(k): v['create'][0]['create'] for k, v in data.items()}
+    print {"{0}_persistence".format(k): v['create'] for k, v in data.items()}
     return {"{0}_persistence".format(k): v.get("create") for k, v in data.items()}
 
 class SubunitParser(testtools.TestResult):
