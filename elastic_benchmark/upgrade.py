b2fee70c07f1fb3749cebfe50c9a954885da7711
lamarwhitej <lamarwhitej@yahoo.com>
2016-11-03 02:19:19 -0500

    Update upgrade.py

--- elastic_benchmark/upgrade.py
+++ elastic_benchmark/upgrade.py
@@ -115,6 +115,7 @@ def parse_persistence(output):
 def parse_project_status(output):
     data = json.loads(open(output).read())
     body = {}
+    print data
 
 class SubunitParser(testtools.TestResult):
     def __init__(self):
@@ -259,5 +260,6 @@ def entry_point():
     differences.update(parse_uptime(cl_args.uptime))
     differences.update(parse_during(cl_args.during))
     differences.update(parse_persistence(cl_args.persistence))
+    parse_project_status(cl_args.status)
     differences.update({"done_time": current_time})
     esc.index(scenario_name='upgrade', env='osa_onmetal', **differences)
