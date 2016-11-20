601cfc6e221373715d250e478c91070da39d2e62
lamarwhitej <lamarwhitej@yahoo.com>
2016-11-20 12:48:19 -0600

    Update upgrade.py

--- elastic_benchmark/upgrade.py
+++ elastic_benchmark/upgrade.py
@@ -292,7 +292,7 @@ def entry_point():
         with open(cl_args.status) as f:
             for line in f:
 		try:
-		    line = line.rstrip('\n')
+		    line = line.rstrip()
 		    line = json.loads(line)
 		    print line
                     esc.index(scenario_name='upgrade_status_log_test', env='osa_onmetal', **line) 
