5a91cc428f724e268e7a95a3df32e937fea8be2a
lamarwhitej <lamarwhitej@yahoo.com>
2016-11-29 15:40:44 -0600

    Update upgrade.py

--- elastic_benchmark/upgrade.py
+++ elastic_benchmark/upgrade.py
@@ -281,7 +281,7 @@ def parse(subunit_file, non_subunit_name="pythonlogging"):
     return subunit_parser
 
 
-def entry_point(self):
+def entry_point():
     current_time = ''
     cl_args = ArgumentParser().parse_args()
     esc = ElasticSearchClient()
