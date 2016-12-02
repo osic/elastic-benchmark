130cd208fc404d9fb03a80c543215aec25a8f972
lamarwhitej <lamarwhitej@yahoo.com>
2016-12-02 14:19:20 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Stephen Lowrie',
     author_email='stephen.lowrie@rackspace.com',
     url='https://github.com/osic/elastic-benchmark',
-    packages=find_packages(),
+    packages=['elastic-benchmark'],
     install_requires=open('requirements.txt').read(),
     classifiers=(
         'Development Status :: 4 - Beta',
