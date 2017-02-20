4c33d2e420a846981e0e284a8357e11a88543206
lamarwhitej <lamarwhitej@yahoo.com>
2017-02-20 16:30:40 -0600

    Update upgrade.py

--- elastic_benchmark/upgrade.py
+++ elastic_benchmark/upgrade.py
@@ -171,11 +171,7 @@ def parse_api_from_status(output):
 	    one_line = json.loads(one_line)
             down_time += one_line['status']
 	line['total_down'] = down_time
-	print line['total_down']
-	print line['duration']
-	uptime_pct = str(round((line['total_down'] / line['duration']) * 100, 1))
-	print "uptime: " + uptime_pct
-	print round((line['total_down'] / line['duration']) * 100, 1)
+	uptime_pct = str(round(((line['duration'] - line['total_down']) / line['duration']) * 100, 1))
     else:
         uptime_pct = str(round(((line['duration'] - line['total_down']) / line['duration']) * 100, 1))
 
